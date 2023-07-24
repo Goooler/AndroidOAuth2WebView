4ccf245656f627b90a1e4a23370314fcc03ca436
Goooler <wangzongler@gmail.com>
2023-07-24 17:54:56 +0800

    Inline consts

--- app/src/main/kotlin/com/example/oauth2webview/MyApplication.kt
+++ app/src/main/kotlin/com/example/oauth2webview/MyApplication.kt
@@ -9,12 +9,6 @@ class MyApplication : Application() {
 
     companion object {
         lateinit var accessTokenManager: OAuth2AccessTokenManager
-
-        // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L143
-        const val CLIENT_ID = "e647013a-ada4-4114-b419-e43d250f99c5"
-
-        // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L147
-        const val REDIRECT_URI = "msauth://com.fsck.k9.debug/VZF2DYuLYAu4TurFd6usQB2JPts%3D"
     }
 
     override fun onCreate() {
@@ -27,9 +21,11 @@ class MyApplication : Application() {
             // https://github.com/thundernest/k-9/blob/00148f1a9939c5d2aa82013007a5386f0b03cab3/app/k9mail/src/main/java/com/fsck/k9/auth/AppOAuthConfigurationFactory.kt#L53-L54
             authorizationEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
             tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-            clientId = CLIENT_ID,
+            // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L143
+            clientId = "e647013a-ada4-4114-b419-e43d250f99c5",
             clientSecret = null,
-            redirectUri = REDIRECT_URI,
+            // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L147
+            redirectUri = "msauth://com.fsck.k9.debug/VZF2DYuLYAu4TurFd6usQB2JPts%3D",
             // https://github.com/thundernest/k-9/blob/00148f1a9939c5d2aa82013007a5386f0b03cab3/app/k9mail/src/main/java/com/fsck/k9/auth/AppOAuthConfigurationFactory.kt#L48-L52
             scope = listOf(
                 "https://outlook.office.com/IMAP.AccessAsUser.All",
