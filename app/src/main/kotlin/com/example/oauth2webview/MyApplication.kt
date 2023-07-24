1416b90c8e958f55d6d6519a2edca3e869ec9d71
Goooler <wangzongler@gmail.com>
2023-07-24 17:25:07 +0800

    Migrate API usages

--- app/src/main/kotlin/com/example/oauth2webview/MyApplication.kt
+++ app/src/main/kotlin/com/example/oauth2webview/MyApplication.kt
@@ -12,7 +12,6 @@ class MyApplication : Application() {
 
         // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L143
         const val CLIENT_ID = "e647013a-ada4-4114-b419-e43d250f99c5"
-        const val CLIENT_SECRET = "CLIENT_SECRET"
 
         // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L147
         const val REDIRECT_URI = "msauth://com.fsck.k9.debug/VZF2DYuLYAu4TurFd6usQB2JPts%3D"
@@ -26,9 +25,10 @@ class MyApplication : Application() {
         accessTokenManager = OAuth2AccessTokenManager(
             storage = storageSharedPreferences,
             // https://github.com/thundernest/k-9/blob/00148f1a9939c5d2aa82013007a5386f0b03cab3/app/k9mail/src/main/java/com/fsck/k9/auth/AppOAuthConfigurationFactory.kt#L53-L54
-            authorizationServerBaseURL = "https://login.microsoftonline.com/common/oauth2/v2.0/",
+            authorizationEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+            tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token",
             clientId = CLIENT_ID,
-            clientSecret = CLIENT_SECRET,
+            clientSecret = null,
             redirectUri = REDIRECT_URI,
             // https://github.com/thundernest/k-9/blob/00148f1a9939c5d2aa82013007a5386f0b03cab3/app/k9mail/src/main/java/com/fsck/k9/auth/AppOAuthConfigurationFactory.kt#L48-L52
             scope = listOf(
