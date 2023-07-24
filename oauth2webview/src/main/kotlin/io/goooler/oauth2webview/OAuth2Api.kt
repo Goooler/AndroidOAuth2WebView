507098b136e9cd58ad92b0189e6a194ce8b28867
Goooler <wangzongler@gmail.com>
2023-07-24 17:14:34 +0800

    Migrate OkHttp APIs

--- oauth2webview/src/main/kotlin/io/goooler/oauth2webview/OAuth2Api.kt
+++ oauth2webview/src/main/kotlin/io/goooler/oauth2webview/OAuth2Api.kt
@@ -3,17 +3,18 @@ package io.goooler.oauth2webview
 import com.google.gson.Gson
 import java.io.IOException
 import java.lang.Exception
-import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 
 /**
  * OkHttp API to communicate with the Authorization Server
  */
 class OAuth2Api {
     private val client = OkHttpClient()
-    private val mediaType = MediaType.get("application/json; charset=utf-8")
+    private val mediaType = "application/json; charset=utf-8".toMediaType()
     private val gson = Gson()
 
     fun requestAccessToken(
@@ -50,7 +51,7 @@ class OAuth2Api {
     }
 
     private fun post(url: String, json: String, callback: (Result<OAuth2AccessToken>) -> Unit) {
-        val body: RequestBody = RequestBody.create(mediaType, json)
+        val body: RequestBody = json.toRequestBody(mediaType)
         val request: Request = Request.Builder()
             .url(url)
             .post(body)
@@ -62,8 +63,8 @@ class OAuth2Api {
                 }
 
                 override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-                    if (response.isSuccessful && response.body() != null) {
-                        val bean = gson.fromJson(response.body()!!.string(), OAuth2AccessToken::class.java)
+                    if (response.isSuccessful && response.body != null) {
+                        val bean = gson.fromJson(response.body!!.string(), OAuth2AccessToken::class.java)
                         callback(Result.success(bean))
                     } else {
                         callback(Result.failure(Exception()))
