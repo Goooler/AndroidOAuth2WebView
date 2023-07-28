df2024671ddd51628860cd4cf919dd973c531249
Goooler <wangzongler@gmail.com>
2023-07-28 12:40:29 +0800

    Add loading layout

--- app/src/main/kotlin/com/example/oauth2webview/LoginActivity.kt
+++ app/src/main/kotlin/com/example/oauth2webview/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.oauth2webview.databinding.ActivityLoginBinding
 import io.goooler.oauth2webview.OAuth2AccessToken
 import io.goooler.oauth2webview.OAuth2Exception
@@ -21,6 +22,8 @@ class LoginActivity : AppCompatActivity() {
             binding.webView,
             object : OAuth2StateListener {
                 override fun onSuccess(token: OAuth2AccessToken) {
+                    binding.flLoading.isVisible = false
+
                     Toast.makeText(this@LoginActivity, "LOGGED", Toast.LENGTH_SHORT).show()
                     Log.d("LoginActivity", token.toString())
 
@@ -29,6 +32,8 @@ class LoginActivity : AppCompatActivity() {
                 }
 
                 override fun onFailure(e: OAuth2Exception) {
+                    binding.flLoading.isVisible = false
+
                     val message = if (e is OAuth2Exception.UserCancelException) e.message else "LOGIN FAILURE"
                     Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
                     e.printStackTrace()
@@ -36,7 +41,9 @@ class LoginActivity : AppCompatActivity() {
                     finish()
                 }
 
-                override fun onLoading() = Unit
+                override fun onLoading() {
+                    binding.flLoading.isVisible = true
+                }
             },
         )
     }
