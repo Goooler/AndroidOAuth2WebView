50d8de04e12b9ad64a52c57de380f5318a5f8d2f
Goooler <wangzongler@gmail.com>
2023-10-05 18:49:52 +0800

    Reuse logic

--- app/src/main/kotlin/com/example/oauth2webview/MyApplication.kt
+++ app/src/main/kotlin/com/example/oauth2webview/MyApplication.kt
@@ -24,6 +24,15 @@ class MyApplication : Application() {
             .addInterceptor(ChuckerInterceptor(this))
             .build()
 
+        val scopes = Stream.of(
+            "https://outlook.office.com/IMAP.AccessAsUser.All",
+            "https://outlook.office.com/SMTP.Send",
+            "offline_access",
+        ).toList()
+        scopes.forEach {
+            Log.d("MyApplication", "scope: $it")
+        }
+
         accessTokenManager = OAuth2AccessTokenManager(
             storage = storageSharedPreferences,
             // https://github.com/thundernest/k-9/blob/00148f1a9939c5d2aa82013007a5386f0b03cab3/app/k9mail/src/main/java/com/fsck/k9/auth/AppOAuthConfigurationFactory.kt#L53-L54
@@ -35,23 +44,10 @@ class MyApplication : Application() {
             // https://github.com/thundernest/k-9/blob/c781e7032d72e8dc457c154bbea6473713861886/app/k9mail/build.gradle.kts#L147
             redirectUri = "msauth://com.fsck.k9.debug/VZF2DYuLYAu4TurFd6usQB2JPts%3D",
             // https://github.com/thundernest/k-9/blob/00148f1a9939c5d2aa82013007a5386f0b03cab3/app/k9mail/src/main/java/com/fsck/k9/auth/AppOAuthConfigurationFactory.kt#L48-L52
-            scope = listOf(
-                "https://outlook.office.com/IMAP.AccessAsUser.All",
-                "https://outlook.office.com/SMTP.Send",
-                "offline_access",
-            ).joinToString(" "),
+            scope = scopes.joinToString(" "),
             client = okHttpClient,
         ).apply {
             prompt = "select_account"
-        }
-
-        val scopes = Stream.of(
-            "https://outlook.office.com/IMAP.AccessAsUser.All",
-            "https://outlook.office.com/SMTP.Send",
-            "offline_access",
-        ).toList()
-        scopes.forEach {
-            Log.d("MyApplication", "scope: $it")
         }
     }
 }
